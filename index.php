321020fd02754c05616edaa819e99c3bd0304fdd
Mantvydas <mantvydas.binderis@gmail.com>
2018-11-04 17:43:56 +0000

    add markup

---
+++ index.php
@@ -0,0 +1,208 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="topnav">
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+                <li>Item 4</li>
+            </ul>
+
+            <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</div>
+        </div>
+    </div><!-- end container -->
+    <header>
+        <div class="container">
+            <nav>
+                <div class="logo">CMC LOGO</div>
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                    <li>Item 4</li>
+                    <li>Item 5</li>
+                    <li>Item 6</li>
+                </ul>
+            </nav>
+
+            <div class="header-text">
+                <h1>Why CMC</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur maiores numquam impedit molestias, voluptatum quaerat corporis quisquam ullam reprehenderit ratione nisi, asperiores eum deserunt, non ea dolores tempora. Et, delectus!</p>
+            </div>
+
+        </div><!-- end container -->
+    </header>
+
+    <div class="bs">
+        <div class="container">
+            <p>Lorem ipsum > Lorem ipsum</p>
+        </div><!-- end container -->
+    </div>
+
+    <main>
+        <div class="container">
+            <section class="intro">
+                    <div class="row">
+                        <div class="col-md-6 circle-section">
+                            <div id="main-wrapper" class="circle-wrapper">
+                                <div id="circle-1" class="circle circle-1"></div>
+                                <div id="circle-2" class="circle circle-2"></div>
+                                <div id="circle-3" class="circle circle-3"></div>
+                                <div id="circle-4" class="circle circle-4"></div>
+                            </div>
+                            
+                        </div>
+
+                        <div class="col-md-6 text-area">
+                            <div class="intro-text text-wrapper">
+                                <div class="h2-wrapper"><h2 class="testh">OUR BUSINESS MODEL</h2></div>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea asperiores iste molestiae tempora eaque, nulla fuga totam ex non eveniet velit omnis aliquid beatae doloremque quas odit? Perferendis, nostrum quia deleniti blanditiis dolores iure aut. Dolores laudantium, aut possimus similique culpa eaque quisquam quidem voluptate harum, doloremque tempora. Provident suscipit ratione voluptatem exercitationem. Voluptatum vitae enim tempore cupiditate autem amet maxime asperiores itaque voluptatem nam reprehenderit dolorem magnam explicabo consequatur nobis aliquid cumque, sequi atque sed alias pariatur animi incidunt?</p>
+                                <div class="btn-wrapper"><button id="anim-btn" class="btn btn-primary">Read More</button></div>
+                            </div>
+
+                            <div id="circle-1-text" class="circle-text text-wrapper">
+                                <div class="h2-wrapper"><h2>CLIENT SERVICE</h2></div>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea asperiores iste molestiae tempora eaque, nulla fuga totam ex non eveniet velit omnis aliquid beatae doloremque quas odit? Perferendis, nostrum quia deleniti blanditiis dolores iure aut. Dolores laudantium, aut possimus similique culpa eaque quisquam quidem voluptate harum, doloremque tempora. Provident suscipit ratione voluptatem exercitationem. Voluptatum vitae enim tempore cupiditate autem amet maxime asperiores itaque voluptatem nam reprehenderit dolorem magnam explicabo consequatur nobis aliquid cumque, sequi atque sed alias pariatur animi incidunt?</p>
+                                <div class="btn-wrapper"><button class="btn btn-primary">Our awwards</button></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="circle-1-border" class="border-bottom"></div>
+            </section>
+
+            <section id="circle-2-section" class="section-border">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="circle-wrapper">
+                            <div id="circle-2-pos" class="circle-hide circle circle-2"></div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div id="circle-2-text" class="circle-text text-wrapper">
+                            <h2>COMPETITIVE PRODUCT PFFERING</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum a quibusdam esse itaque commodi ratione vero voluptates, delectus minima debitis eveniet suscipit. Est quas modi iste maiores quasi, saepe possimus quidem nulla officiis blanditiis quos repellendus voluptatum. Nobis blanditiis quam aut, impedit voluptas nam a odit quo ducimus sit, quia unde voluptatibus aliquam quibusdam nemo alias excepturi consectetur mollitia? Nostrum nihil beatae, debitis animi magnam, maxime aperiam porro, odio voluptas sapiente voluptatibus minus laudantium vel! Commodi reprehenderit corrupti cupiditate placeat natus ipsam ut eum blanditiis fugit est. Modi saepe eos maiores quam, quaerat doloremque quis reprehenderit blanditiis repellendus commodi tempora!</p>
+                            <button class="btn btn-primary">Read More</button>
+                        </div>
+                    </div>
+
+                </div>
+                <div id="circle-2-border" class="border-bottom"></div>
+            </section>
+
+             <section id="circle-3-section" class="section-border">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="circle-wrapper">
+                            <div id="circle-3-pos" class="circle-hide circle circle-3"></div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div id="circle-3-text" class="text-wrapper">
+                        <h2>TECHNOLOGY AND OPERATIONAL EXCELLENCE</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum a quibusdam esse itaque commodi ratione vero voluptates, delectus minima debitis eveniet suscipit. Est quas modi iste maiores quasi, saepe possimus quidem nulla officiis blanditiis quos repellendus voluptatum. Nobis blanditiis quam aut, impedit voluptas nam a odit quo ducimus sit, quia unde voluptatibus aliquam quibusdam nemo alias excepturi consectetur mollitia? Nostrum nihil beatae, debitis animi magnam, maxime aperiam porro, odio voluptas sapiente voluptatibus minus laudantium vel! Commodi reprehenderit corrupti cupiditate placeat natus ipsam ut eum blanditiis fugit est. Modi saepe eos maiores quam, quaerat doloremque quis reprehenderit blanditiis repellendus commodi tempora!</p>
+                        <button class="btn btn-primary">Read More</button>
+                        </div>
+                    </div>
+
+                </div>
+                <div id="circle-3-border" class="border-bottom"></div>
+            </section>
+
+             <section id="circle-4-section" class="section-border">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="circle-wrapper">
+                            <div id="circle-4-pos" class="circle-hide circle circle-4"></div>
+                        </div>
+                    </div>
+
+                    <div id="circle-4-text" class="col-md-6">
+                        <div class="text-wrapper">
+                        <h2>OUR PEOPLE</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum a quibusdam esse itaque commodi ratione vero voluptates, delectus minima debitis eveniet suscipit. Est quas modi iste maiores quasi, saepe possimus quidem nulla officiis blanditiis quos repellendus voluptatum. Nobis blanditiis quam aut, impedit voluptas nam a odit quo ducimus sit, quia unde voluptatibus aliquam quibusdam nemo alias excepturi consectetur mollitia? Nostrum nihil beatae, debitis animi magnam, maxime aperiam porro, odio voluptas sapiente voluptatibus minus laudantium vel! Commodi reprehenderit corrupti cupiditate placeat natus ipsam ut eum blanditiis fugit est. Modi saepe eos maiores quam, quaerat doloremque quis reprehenderit blanditiis repellendus commodi tempora!</p>
+                        <button class="btn btn-primary">Read More</button>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+        </div><!-- end container -->
+    </main>
+
+    <footer>
+        <div class="container">
+            <div class="f-r">
+                <div>CMC LOGO</div>
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                    <li>Item 4</li>
+                    <li>Item 5</li>
+                    <li>Item 6</li>
+                </ul>
+            </div>
+
+            <div class="f-r">
+                <div>
+                    Lorem, ipsum dolor.<br>
+                    Lorem, ipsum dolor.<br>
+                    Lorem, ipsum dolor.
+                </div>
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                </ul>
+            </div>
+
+            <div class="f-r third-row">
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                    <li>Item 4</li>
+                    <li>Item 5</li>
+                    <li>Item 6</li>
+                </ul> 
+
+                <div>Lorem, ipsum.</div>
+            </div>
+
+            <div class="f-r fourth-row">
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                    <li>Item 4</li>
+                </ul> 
+
+                <div>Lorem, ipsum.  Lorem, ipsum.</div>
+            </div>
+
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quasi, nostrum facilis pariatur quam expedita in, nesciunt aperiam quaerat praesentium quisquam quae vitae consectetur nisi ducimus cumque quibusdam obcaecati fugit velit vel. Est dolorum architecto, reiciendis excepturi possimus voluptas sit? In asperiores quisquam tempora, odio ratione dignissimos veritatis error doloribus Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio labore repellendus id eius ea delectus dicta non, ut iusto sapiente, ab voluptatibus! Aspernatur consectetur, labore veniam animi corrupti eius porro hic omnis velit repellat! A expedita, ratione accusantium nisi ut dolorem dolore eos quia distinctio sint, nobis in assumenda! Repudiandae doloremque delectus voluptatum nostrum fuga quisquam? Neque sit, cum saepe vitae sed, iusto blanditiis consectetur corporis fuga maiores rerum commodi nam officia? Dolorum iure, sit ipsum illum distinctio recusandae mollitia!.</p>
+        </div><!-- end container -->
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TweenMax.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/ScrollMagic.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/plugins/debug.addIndicators.min.js"></script>
+    <script src="plugins/animation.gsap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TweenMax.min.js"></script>
+    <script src="script.js"></script>
+</body>
+</html>
